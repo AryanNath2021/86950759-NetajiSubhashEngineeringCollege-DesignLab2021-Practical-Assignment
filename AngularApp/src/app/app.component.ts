d930edc41381a1ca96daa541e0e7998f5bec62d7
AryanNath2021 <aryann.cse2017@nsec.ac.in>
2021-07-05 13:47:28 +0530

    Initial commit

---
+++ AngularApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  //templateUrl: './app.component.html',
+  template:` <app-employee></app-employee> `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularApp';
+}
---
+++ AngularApp/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { RouterModule } from '@angular/router';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
